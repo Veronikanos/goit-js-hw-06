7be4148efb9eaa1c88caa71d4e7c91f94d05dd99
Veronica <vveronikanika@gmail.com>
2022-11-01 16:56:39 +0200

    feat: task 5

---
+++ src/js/task-05.js
@@ -0,0 +1,9 @@
+const inputForm = document.querySelector('input#name-input');
+const nameOutput = document.querySelector('span#name-output');
+const showInputName = (e) => {
+	console.log(e.currentTarget.value);
+	nameOutput.textContent = e.currentTarget.value ? e.currentTarget.value : "Anonymous";
+};
+
+inputForm.addEventListener('input', showInputName);
+
