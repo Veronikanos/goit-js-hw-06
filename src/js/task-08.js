64dd2928f4a1c35979466f344c24aa810a135510
Veronica <vveronikanika@gmail.com>
2022-11-01 20:00:56 +0200

    feat: task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
 
+const formElement = document.querySelector('form.login-form');
+
+function onSubmitForm(e) {
+	e.preventDefault();
+
+	const formElements = e.currentTarget.elements;
+	if (!formElements.email.value || !formElements.password.value){
+		alert('Bсі поля повинні бути заповнені');
+	} else {
+		const dataFormInput = {
+			mail: formElements.email.value,
+			password: formElements.password.value,
+		};
+		console.log(dataFormInput);
+		formElement.reset();
+	}
+}
+
+formElement.addEventListener('submit', onSubmitForm);
