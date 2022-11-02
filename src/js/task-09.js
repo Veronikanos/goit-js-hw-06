df23d426ac48c380b2504286ae98c09c2d7e25a0
Veronica <vveronikanika@gmail.com>
2022-11-02 12:19:56 +0200

    feat: task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonChangeColor = document.querySelector('button.change-color');
+const body = document.body;
+const bgColorSpanElement = document.querySelector('span.color');
+
+function onButtonClick(){
+	const newColor = getRandomHexColor();
+	body.style.backgroundColor = newColor;
+	bgColorSpanElement.textContent = newColor;
+}
+
+buttonChangeColor.addEventListener('click', onButtonClick);
